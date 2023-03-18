23368bde657518f2b954d3cc9e0b17ded36c7076
Ruba_Bakhour <rabdullahbakhour@stu.kau.edu.sa>
2023-03-18 06:07:25 +0300

    First commit

---
+++ src/lab1_cpit251/Lab1_CPIT251.java
@@ -0,0 +1,19 @@
+
+package lab1_cpit251;
+
+/**
+ *
+ * @author Ruba
+ */
+public class Lab1_CPIT251 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hello! This java file is made for Lab 1.");
+        
+    }
+    
+}
